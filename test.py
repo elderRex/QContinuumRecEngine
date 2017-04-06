6b5be69aee89d34d858f25026a44b4d90eb9a859
NickG <yu.gu@columbia.edu>
2017-04-06 16:16:59 -0400

    test suite update v0.1

--- test.py
+++ test.py
@@ -3,6 +3,10 @@ import QCRecommendation.QC_Prediction_Engine_Flask.db_initialization as dbi
 import QCRecommendation.QC_Prediction_Engine_Flask.NLP_processing.NLPblock as nlpb
 import QCRecommendation.QC_Prediction_Engine_Flask.NLP_processing.NLPmain as nlpm
 
+import os
+import flaskr
+import tempfile
+
 class testcase(unittest.TestCase):
 
     def test_db(self):
@@ -26,7 +30,7 @@ class testcase(unittest.TestCase):
             # print row[0],row[2]
             # item,label
             blc.answers.append((row[1], row[2]))
-        print [y for (x, y) in blc.answers]
+        #print [y for (x, y) in blc.answers]
         for row in test_reviews:
             # print row
             blc.batch_test.append((row[3], row[4]))
@@ -34,7 +38,19 @@ class testcase(unittest.TestCase):
 
     def test_main(self):
         self.assertIsNotNone(nlpm.main_func(84))
+'''
+class FlaskrTestCase(unittest.TestCase):
 
+    def setUp(self):
+        self.db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
+        flaskr.app.config['TESTING'] = True
+        self.app = flaskr.app.test_client()
+        with flaskr.app.app_context():
+            flaskr.init_db()
 
+    def tearDown(self):
+        os.close(self.db_fd)
+        os.unlink(flaskr.app.config['DATABASE'])
+'''
 if __name__ == '__main__':
     unittest.main()
